83ab13c5c35dd5fb62b0bc18f4f042cbb8882ac4
Trần Vinh Nguyễn <vinh.nguyentrandev@gmail.com>
2025-07-23 19:47:18 +0700

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
   // resolve: {
   //   alias: {
