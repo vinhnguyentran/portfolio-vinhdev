512872d2a85bbcb5a91c73712dd36596c07a1f91
Trần Vinh Nguyễn <vinh.nguyentrandev@gmail.com>
2025-07-23 19:52:12 +0700

    update vite file 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Toaster/>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           
           <Route path='*' element={<NotFound/>}/>
          
