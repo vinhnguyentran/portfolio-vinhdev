2b9f4e105d27f0b8cf0fbc555a30e118207889a1
Trần Vinh Nguyễn <vinh.nguyentrandev@gmail.com>
2025-07-23 18:47:24 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   }
+  // }
 })
